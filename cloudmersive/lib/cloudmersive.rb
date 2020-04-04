e33fb69fb1a1445d27ed475b52af3efc356f8aa1
drewsview34 <drewsview34@gmail.com>
2020-04-04 12:04:45 -0700

    added cloudmersive gem

---
+++ cloudmersive/lib/cloudmersive.rb
@@ -0,0 +1,6 @@
+require "cloudmersive/version"
+
+module Cloudmersive
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ cloudmersive/lib/cloudmersive/version.rb
@@ -0,0 +1,3 @@
+module Cloudmersive
+  VERSION = "0.1.0"
+end
